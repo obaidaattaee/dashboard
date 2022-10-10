132650e399ec813f459bacfd5799b0a2982bff2f
obaidaattaee <obaataii@gmail.com>
2022-10-10 15:36:40 +0200

    default string length in database

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(125);
 
         if (Schema::hasTable('settings')) {
 
